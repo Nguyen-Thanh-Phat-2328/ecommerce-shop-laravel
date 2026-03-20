f1b4083ef87ed450509f7f122476bc42822ebe30
Nguyen Thanh Phat <23042004phat@gmail.com>
2026-03-20 11:46:10 +0700

    create ui admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.dashboard');
+// });
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index']);
+Route::get('/admin/profile', [UserController::class, 'index']);
